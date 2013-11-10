de5de11a68cd20416394214e189cdf57eb3d4591
stevesun21 <SteveSunCanada@gmail.com>
2013-11-10 11:01:58 -0600

    added license header

--- src/example/org/gogoup/dddutils/SimpleHttpServer.java
+++ src/example/org/gogoup/dddutils/SimpleHttpServer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Rui Sun (SteveSunCanada@gmail.com)
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package example.org.gogoup.dddutils;
 
 import java.util.Map;
@@ -23,7 +39,7 @@ import org.gogoup.dddutils.remote.server.impl.DefaultHttpServer;
 
 public class SimpleHttpServer {
 	
-	private static final boolean NEED_SESSION_PERSISTENCE = false; //<--- Setup MySQL Connection before change this to true.
+	private static final boolean NEED_SESSION_PERSISTENCE = true; //<--- Setup MySQL Connection before change this to true.
 	
 	public SimpleHttpServer() {
 		// TODO Auto-generated constructor stub
